1104af3b440f6ccf38fd6a38f37dc6a37e68e7ae
a1280034 <87135485+a1280034@users.noreply.github.com>
2021-07-09 00:18:19 +0900

    Create dicegame2.c

---
+++ dicegame2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    char name[10];
+    int i;
+    int total=0;
+    srand(time(NULL));
+    printf("What is your name?\n> ");
+    scanf("%s",name);
+    printf("Hello %s\n",name);
+    printf("Rolling the Dice...\n");
+    for(i=0;i<3;i++){
+        printf("Dice %d: ",i+1);
+        int a= rand() % 6 + 1;
+        total+=a;
+        printf("%d\n",a);
+    }
+    printf("Total value : %d\n",total);
+    if(total>14) printf("%s won!!\n",name);
+    else printf("%s lost!\n",name);
+    return 0;
+}
